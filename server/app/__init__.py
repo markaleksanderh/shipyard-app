cc98bf4be041f5337a548bf0842b9c84a3b6c06a
Mark Hil <aleksanderhil@gmail.com>
2021-01-19 10:12:40 +0000

    Added config, updated Dockerfile

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,4 +1,10 @@
-from flask import Flask
+from config import Config
+
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+
+
 
 def create_app():
     app = Flask(__name__)
